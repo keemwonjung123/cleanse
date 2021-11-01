79efd22ff02dd36a6ca542403563b7c7fd584de7
Micheal Kim <m3nukim@kakao.com>
2021-11-02 04:07:49 +0900

    Support Windows High DPI Scaling

--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
 # cleanse 3.0
 # main.py
 
-import sys
+import sys, os
+from platform import system as get_platform
 from PyQt5.QtWidgets import *
 from cleanse_window import CleanseWindow
 
 
 # main
 if __name__ == "__main__":
+    if get_platform() == "Windows":
+        os.environ["QT_ENABLE_HIGHDPI_SCALING"] = "1"
     app = QApplication(sys.argv)
     cleanse_window = CleanseWindow()
     cleanse_window.show()
